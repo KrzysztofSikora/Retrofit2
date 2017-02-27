e664180bd63df967d10407c27a19e875dde34565
krzysztof <listdlakrzycha@gmail.com>
2017-02-27 19:08:11 +0100

    Initial commit

---
+++ app/src/main/java/pl/krzysztofsikora/retrofit2/data/remote/ApiUtils.java
@@ -0,0 +1,15 @@
+package pl.krzysztofsikora.retrofit2.data.remote;
+
+
+public class ApiUtils {
+
+    private ApiUtils() {}
+
+//    public static final String BASE_URL = "http://jsonplaceholder.typicode.com/";
+    public static final String BASE_URL = "http://192.168.1.103:3000/";
+
+    public static APIService getAPIService() {
+
+        return RetrofitClient.getClient(BASE_URL).create(APIService.class);
+    }
+}
